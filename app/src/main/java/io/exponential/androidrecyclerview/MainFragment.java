542887267a18fd9d6dc894b3f952b3824fc626c8
akbarahmed <akbar501@gmail.com>
2015-06-10 09:26:45 -0700

    create RecyclerView instance in MainFragment

--- app/src/main/java/io/exponential/androidrecyclerview/MainFragment.java
+++ app/src/main/java/io/exponential/androidrecyclerview/MainFragment.java
@@ -3,13 +3,21 @@ package io.exponential.androidrecyclerview;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainFragment extends Fragment {
-    private Callbacks mListener;
+    private Callbacks callbacks;
+
+    private RecyclerView carsRecyclerView;
+    private CarAdapter carAdapter;
 
     public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
@@ -25,11 +33,38 @@ public class MainFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    private static List<Car> getCarsDataset() {
+        List<Car> cars = new ArrayList<>();
+
+        String[] makes = {"Ford", "GM", "BMW", "Toyota", "Honda", "Mercedes"};
+        String[] models = {"F150", "Corvette", "M8", "Camry", "Accord", "S65"};
+
+        for (int i = 0; i < makes.length && i < models.length; i++) {
+            Car car = new Car(makes[i], models[i]);
+            cars.add(car);
+        }
+
+        return cars;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get a reference to the RecyclerView element in the Fragment's layout
+        carsRecyclerView = (RecyclerView) view.findViewById(R.id.cars);
+
+        // Get the cars dataset
+        List<Car> carsDataset = getCarsDataset();
+
+        // Set the adapter
+        carAdapter = new CarAdapter(carsDataset);
+        carsRecyclerView.setAdapter(carAdapter);
+
+        // Set the layout manager
+        carsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
     }
@@ -38,7 +73,7 @@ public class MainFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mListener = (Callbacks) activity;
+            callbacks = (Callbacks) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString() + " must implement Callbacks");
         }
@@ -47,7 +82,7 @@ public class MainFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        callbacks = null;
     }
 
     public interface Callbacks {
