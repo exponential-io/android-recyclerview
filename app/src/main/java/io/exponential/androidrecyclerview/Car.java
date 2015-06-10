0bcd1e7ab26f7bb4e4d14c196cedf0ffb36c5627
akbarahmed <akbar501@gmail.com>
2015-06-10 01:44:46 -0700

    create data model

---
+++ app/src/main/java/io/exponential/androidrecyclerview/Car.java
@@ -0,0 +1,14 @@
+package io.exponential.androidrecyclerview;
+
+/**
+ * Simple model
+ */
+public class Car {
+    private String make;
+    private String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+}
